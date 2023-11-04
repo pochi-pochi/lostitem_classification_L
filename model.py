69415bdf3db7b27e2dc7b971727b801b1937d0fb
pochi-pochi <takahiroa500@outlook.jp>
2023-11-04 19:54:28 +0900

    [add]モデルの構築
    VGG-16を転移学習させるコードを制作

--- model.py
+++ model.py
@@ -1,4 +1,97 @@
 import torch.nn as nn
 import torch.optim as optim
-from torchvision import transforms
+from torchvision import transforms, datasets, models
 from torch.utils.data import DataLoader
+from torch.utils.data import random_split
+from tqdm import tqdm
+import torch
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(f"Using {device} device")
+
+# 前処理クラス
+transform = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.ToTensor(),
+])
+
+
+# Datasetの作製
+Data_folder = './dataset/train'
+dataset = datasets.ImageFolder(root=Data_folder, transform=transform)
+
+train_size = int(0.8 * len(dataset))
+test_size = len(dataset) - train_size
+
+train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
+
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=32, shuffle=True)
+dataloaders_dict = {'train': train_loader, 'test': test_loader}
+
+
+# モデルの定義
+use_pretrained = True
+net = models.vgg16(pretrained=use_pretrained)
+
+net.classifier[6] = nn.Linear(in_features=4096, out_features=26)
+net = net.to(device)
+
+net.train()
+
+print('ネットワークの訓練準備完了')
+
+
+# 損失関数、最適化手法を定義
+criterion = nn.CrossEntropyLoss()
+
+optimizer = optim.SGD(net.parameters(), lr=0.001)
+
+
+# モデル学習用関数
+def train_model(net, data_loaders_dist, criterion, optimizer, num_epochs):
+    net.to(device)
+    for epoch in range(num_epochs):
+        print(f'{epoch+1}/{num_epochs}')
+        print('----------------------------')
+
+        for phase in ['train', 'test']:
+            if phase == 'train':
+                net.train()
+            else:
+                net.eval()
+
+            epoch_loss = 0.0
+            epoch_corrects = 0
+
+            if (epoch == 0) and (phase == 'train'):
+                continue
+
+            for inputs, labels in tqdm(data_loaders_dist[phase]):
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+                optimizer.zero_grad()
+
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = net(inputs)
+                    loss = criterion(outputs, labels)
+                    _, preds = torch.max(outputs, 1)
+
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                    epoch_loss += loss.item() * inputs.size(0)
+                    epoch_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = epoch_loss / len(data_loaders_dist[phase].dataset)
+            epoch_acc = epoch_corrects.double(
+                ) / len(data_loaders_dist[phase].dataset)
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss,
+                                                       epoch_acc))
+
+
+# 学習の実行
+num_epochs = 10
+train_model(net, dataloaders_dict, criterion, optimizer, num_epochs=num_epochs)
--- one_shot.py
+++ one_shot.py
@@ -17,40 +17,69 @@ url = "https://www.google.co.jp/imghp?hl=ja"
 browser.get(url)
 
 # 保存先のディレクトリ
-base_directory = "dataset/train/Bags"
+base_directory = "dataset/train/"
 
 # 検索キーワードとしてフォルダ名を使用
-actor_name = "Bags"
+actor_name = "お金"
+dir_name = "cash"
 
 selector = "body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > textarea"
 kw_search = browser.find_element(By.CSS_SELECTOR, selector)
 kw_search.clear()
 kw_search.send_keys(str(actor_name))
 kw_search.send_keys(Keys.ENTER)
+SCROLL_PAUSE_TIME = 2  # 2秒待機。必要に応じて調整可能
 
-# BeautifulSoupで画像検索したページの画像を取得する
-cur_url = browser.current_url
-res = requests.get(cur_url)
-soup = BeautifulSoup(res.text, "html5lib")
 
-img_tags = soup.find_all("img", limit=100)
+# 一定の回数スクロールする
+for _ in range(3):
+    # スクロールダウン実行
+    browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    # 画像の読み込みや「もっと見る」ボタンが現れるのを待つ
+    sleep(SCROLL_PAUSE_TIME)
+
+    # 「もっと見る」ボタンがあればクリックする
+    try:
+        more_button = browser.find_element_by_css_selector("ボタンのCSSセレクタ")
+        more_button.click()
+        sleep(SCROLL_PAUSE_TIME)
+    except:
+        pass
+
+# スクロールが終わったら、そのページの内容をBeautifulSoupに取り込む
+html = browser.page_source
+soup = BeautifulSoup(html, "html.parser")
+
+img_tags = soup.find_all("img")
 img_urls = []
 
 for img_tag in img_tags:
     url_a = img_tag.get("src")
     if url_a != None:
         img_urls.append(url_a)
+# # BeautifulSoupで画像検索したページの画像を取得する
+# cur_url = browser.current_url
+# res = requests.get(cur_url)
+# soup = BeautifulSoup(res.text, "html5lib")
+
+# img_tags = soup.find_all("img", limit=100)
+# img_urls = []
+
+# for img_tag in img_tags:
+#     url_a = img_tag.get("src")
+#     if url_a != None:
+#         img_urls.append(url_a)
 
 # 画像を指定のフォルダに保存
-save_dir = os.path.join(base_directory, actor_name)
+save_dir = os.path.join(base_directory, dir_name)
 if not os.path.exists(save_dir):
     os.mkdir(save_dir)
 a = 1
 for elem_url in img_urls:
     try:
         r = requests.get(elem_url)
-        with open(os.path.join(save_dir,
-                                f"{actor_name}画像{a}.jpg"), "wb") as fp:
+        with open(os.path.join(save_dir, f"{dir_name}画像{a}.jpg"), "wb") as fp:
             fp.write(r.content)
         a += 1
         sleep(0.1)
